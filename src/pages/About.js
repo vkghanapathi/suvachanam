70b0fc41c3f9d28ef3aae6300efe7f7ddfb05e75
Dr. Vamshi Krishna Ghanapaathi <sarvaveda@gmail.com>
2025-11-21 20:36:58 +0530

    Add About page

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <main>
+      <h2>About Us</h2>
+      <p>Suvachanam is an online journal dedicated to insightful articles on various topics.</p>
+    </main>
+  );
+};
+
+export default About;
